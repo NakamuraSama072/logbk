8e0f0fb8c31aba462a624aa939fc5b736af3a6e9
NakamuraSama072 <stephanwilliamson@qq.com>
2026-02-03 15:22:21 +0800

    chore!: Added GPLv3 declaration according to its requriements

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+/*
+logbk - A simple C++ log backup program
+Copyright (C) 2026  NakamuraSama072
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "include/utils.h"
 #include "include/bktool.h"
 #include "include/prjcli.h"
@@ -16,6 +34,10 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     if (std::string(argv[1]) == "help") {
+        std::cerr << "logbk  Copyright (C) 2026  NakamuraSama072\n";
+        std::cerr << "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n";
+        std::cerr << "This is free software, and you are welcome to redistribute it\n";
+        std::cerr << "under certain conditions; type `show c' for details.\n";
         show_help(argv[0]);
         return 0;
     }
